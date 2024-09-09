0e702705f50dea1d138fb4842d8e8121dae2d340
Gagan <gkaur21.rrc.ca>
2024-09-09 10:50:57 -0500

    Experimented with built-in-functions.

--- module_02.py
+++ module_02.py
@@ -16,16 +16,35 @@ It can span multipe lines.
 
 """
 
+absolute_value = abs(-12) # abs is absolute and ()Round brackets means A Function
+print('Absolute value:', absolute_value) # '' means Text Literal
 
 
+print('Absolute value:', abs(-12))
 
+print("I am ", 25, "years old.")
+print('I am ',25, 'years old.')
+print("I am 20 years old.")
+print(25)
 
+print("Apples","Oranges","Banana")
+print("Apples","Oranges","Banana", sep= ",")
+print("Apples","Oranges","Banana", sep= "")
 
+print("Print usinf f-string")
+name = "John"
+age = 25
 
+print("My name is ",name,"and I am ",age,"years old.")
 
+print(f"My name is {name} and I am {age} years old")
+value = 3.149159
+print(f"The value is {value}")
 
+print(f"The value is {value:.2f}") # Rounding 
 
+number = 123;
+print(f"The number is {number:05}.") # Add Numbers
 
-
-
-
+print(f"Hello, {name}")
+print(f"Hello, {name:>10}") # Make this 10 characters long ...
